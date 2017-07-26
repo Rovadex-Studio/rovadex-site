6a374f0a79b5877cd56da281a3c80750db26dc49
Denis <makhonkodenis@gmail.com>
2017-07-26 22:02:37 +0300

    ADD: singe team page style

--- config/thumbnails.php
+++ config/thumbnails.php
@@ -10,5 +10,5 @@ return array(
 	'post-thumbnail'        => array( 'width' => 886, 'height' => 552, 'crop' => true ),
 	'post-list-thumbnail'   => array( 'width' => 940, 'height' => 670, 'crop' => true ),
 	'team-thumbnail'        => array( 'width' => 380, 'height' => 720, 'crop' => true ),
-	'team-thumbnail-single' => array( 'width' => 380, 'height' => 720, 'crop' => true ),
+	'team-thumbnail-single' => array( 'width' => 200, 'height' => 200, 'crop' => true ),
 );

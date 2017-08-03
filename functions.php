f592a8e60afb08046a6828a70036dc325abba69c
Denis <makhonkodenis@gmail.com>
2017-08-03 15:12:53 +0300

    UPD: compress assets

--- functions.php
+++ functions.php
@@ -239,21 +239,20 @@ if ( ! class_exists( 'Rovadex_Theme' ) ) {
 			wp_enqueue_style( 'font-awesome', $this->assets_url() . 'css/font-awesome.min.css', array(), '2.2.0' );
 			wp_enqueue_style( 'material-icons', $this->assets_url() . 'css/material-icons.min.css', array(), '2.2.0' );
 			wp_enqueue_style( 'jquery.fullpage', $this->assets_url() . 'css/jquery.fullpage.min.css', array(), '2.9.4' );
+			wp_enqueue_style( 'owl.carousel', $this->assets_url() . 'css/owl.carousel.min.css', array(), '3.4.2' );
 			wp_enqueue_style( 'rovadex-style', get_stylesheet_uri() );
 
 			// Register JavaScripts.
-			wp_enqueue_script( 'rovadex-tween-max', $this->assets_url() . 'js/TweenMax.js', array( 'jquery' ), '1.19.1', true );
-			wp_enqueue_script( 'rovadex-timeline-max', $this->assets_url() . 'js/TimelineMax.js', array( 'jquery' ), '1.19.1', true );
-			wp_enqueue_script( 'rovadex-ease-pack', $this->assets_url() . 'js/EasePack.js', array( 'jquery' ), '1.15.5', true );
+			wp_enqueue_script( 'rovadex-tween-max', $this->assets_url() . 'js/min/TweenMax.min.js', array( 'jquery' ), '1.19.1', true );
+			wp_enqueue_script( 'rovadex-timeline-max', $this->assets_url() . 'js/min/TimelineMax.min.js', array( 'jquery' ), '1.19.1', true );
+			wp_enqueue_script( 'rovadex-ease-pack', $this->assets_url() . 'js/min/EasePack.min.js', array( 'jquery' ), '1.15.5', true );
 
-			wp_enqueue_script( 'jquery-cherry-responsive-menu', $this->assets_url() . 'js/cherry-responsive-menu.js', array( 'jquery' ), '1.0.0', true );
+			wp_enqueue_script( 'jquery-cherry-responsive-menu', $this->assets_url() . 'js/min/cherry-responsive-menu.min.js', array( 'jquery' ), '1.0.0', true );
 			wp_enqueue_script( 'rovadex-theme-script', $this->assets_url() . 'js/theme-script.js', array(), '1.0.0', true );
 
 			if ( is_front_page() ) {
 				wp_enqueue_script( 'jquery.fullpage.extensions', $this->assets_url() . 'js/min/jquery.fullpage.extensions.min.js', array(), '2.9.4', true );
 				wp_enqueue_script( 'jquery.fullpage', $this->assets_url() . 'js/min/jquery.fullpage.min.js', array(), '2.9.4', true );
-
-				wp_enqueue_style( 'owl.carousel', $this->assets_url() . 'css/owl.carousel.min.css', array(), '3.4.2' );
 				wp_enqueue_script( 'owl.carousel', $this->assets_url() . 'js/min/owl.carousel.min.js', array( 'jquery' ), '3.4.2', true );
 			}
 
